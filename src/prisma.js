ac3d51b9a895ac279983082b65d2f52e61edaeaa
aloksingh3112 <aloksingh3112@gmail.com>
2018-11-08 23:00:18 +0530

    added prisma query and mutation feature

--- src/prisma.js
+++ src/prisma.js
@@ -27,13 +27,48 @@ const prisma=new Prisma({
 // } ,'{id title body isPublish user{id name email}}')
 //   .then(data=>console.log(data))
 
- prisma.mutation.updatePost({
-     data:{
-         body:"i like u",
-         isPublish:false
-     },
-     where:{
-         id:"cjo8crlmz001a0868yi2zg07m"
-     }
- },'{id title body isPublish}')
- .then(data=>console.log(data));
+//  prisma.mutation.updatePost({
+//      data:{
+//          body:"i like u",
+//          isPublish:false
+//      },
+//      where:{
+//          id:"cjo8crlmz001a0868yi2zg07m"
+//      }
+//  },'{id title body isPublish}')
+//  .then(data=>console.log(data));
+
+const updatePostForUser=async (postId,data)=>{
+    try{   
+      const userExist=await prisma.exists.Post({
+          id:postId
+      })  
+      if(!userExist){
+          throw new Error("new post available");
+      }  
+    const updatedPost=await prisma.mutation.updatePost({
+        data:{
+            ...data
+        },
+        where:{
+            id:postId
+        }
+    }
+        ,'{ user {id} }');
+
+   const user=await prisma.query.user({
+       where:{
+           id:updatedPost.user.id
+       }
+   },'{id name email}')
+   return user
+}catch(err){
+    console.log("hggg",err);
+}
+}
+updatePostForUser('cjo8sfczc0008086875r8r1e3',{
+    title:"hello worls i m here"
+}).then(data=>console.log(data));
+
+
+
