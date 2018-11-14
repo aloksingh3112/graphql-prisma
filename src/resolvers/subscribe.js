80eb966f445179e16547efafa2b302dd1db2ebc9
aloksingh3112 <aloksingh3112@gmail.com>
2018-11-15 02:27:46 +0530

    added subscription to post and comment

--- src/resolvers/subscribe.js
+++ src/resolvers/subscribe.js
@@ -1,34 +1,36 @@
 const Subscription={
-  count:{
-      subscribe(parent,args,{pubsub},info){
-          let count=0;
-          setInterval(()=>{
-             count++;
-             pubsub.publish('count',{
-                 count
-             })
-          },1000)
-          return pubsub.asyncIterator('count');
+//   count:{
+//       subscribe(parent,args,{pubsub},info){
+//           let count=0;
+//           setInterval(()=>{
+//              count++;
+//              pubsub.publish('count',{
+//                  count
+//              })
+//           },1000)
+//           return pubsub.asyncIterator('count');
 
-      }
-  },
+//       }
+//   },
 
   comment:{
-      subscribe(parent,{postId},{db,pubsub},info){
+      subscribe(parent,{postId},{prisma},info){
 
-        const post=db.posts.find((post)=>post.id==postId && post.isPublished);
-        if(!post){
-            throw new Error("no post found");
-        }
+        return prisma.subscription.comment(null,info)
 
-        return pubsub.asyncIterator(`comment ${postId}`);
+        // const post=db.posts.find((post)=>post.id==postId && post.isPublished);
+        // if(!post){
+        //     throw new Error("no post found");
+        // }
+
+        // return pubsub.asyncIterator(`comment ${postId}`);
 
       }
   },
 
   post:{
-      subscribe(parent,args,{pubsub},info){
-          return pubsub.asyncIterator("post1");
+      subscribe(parent,args,{prisma},info){
+        return prisma.subscription.post(null,info);
       }
   }
 }
